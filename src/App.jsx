464d316f9d3879633146f4ea6d401531d6498f7e
Maksim <urchenkomaksim@gmail.com>
2025-11-08 07:39:45 +0200

    Added Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Header from './components/Header.jsx';
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header />
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+﻿import logo from '../assets/investment-calculator-logo.png';
+
+function Header() {
+  return (
+    <header id="header">
+      <img src={logo} alt="Money bag logo" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
+
+export default Header;
