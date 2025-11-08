4aef810ff3e9dcc2cfbd318ee00398abecb815ce
Maksim <urchenkomaksim@gmail.com>
2025-11-08 07:45:09 +0200

    Addedd UserInput markup

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,28 @@
+﻿function UserInput() {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investments</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Anual Investments</label>
+          <input type="number" required />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input type="number" required />
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default UserInput;
